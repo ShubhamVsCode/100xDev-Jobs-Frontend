4d7a09edc26fe1a8cc5578c18382641f723bbbb4
Shubham <shubhamvscode@gmail.com>
2023-11-27 01:19:40 +0530

    refactor: Removed Uploadthing for profile image - issue: no-id

--- src/components/profile/UserInfoForm.tsx
+++ src/components/profile/UserInfoForm.tsx
@@ -1,12 +1,15 @@
 import FormWrapper from "./FormWrapper";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { UploadDropzone, Uploader } from "@/utils/uploadthing";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import useUserStore from "@/store/user-store";
 import { UseFormGetValues, UseFormSetValue } from "react-hook-form";
 import { ProfileType } from "@/lib/validations/auth";
+import { useDropzone } from "react-dropzone";
+import { getObjectURL } from "@/lib/utils";
+import { Button } from "../ui/button";
+import UploadAPI from "@/api/upload";
 
 type StepProps = {
   updateForm: UseFormSetValue<ProfileType>;
@@ -14,51 +17,120 @@ type StepProps = {
 };
 
 const UserInfoForm = ({ updateForm, getValues }: StepProps) => {
-  const [tempProfilePicture, setTempProfilePicture] = useState(
-    getValues("picture")
-  );
+  const [profilePicture, setProfilePicture] = useState<{
+    uploaded: string | null;
+    notUploaded: File | null;
+  }>({
+    uploaded: getValues("picture") || null,
+    notUploaded: null,
+  });
+
+  useEffect(() => {
+    if (!getValues("picture")) return;
+    setProfilePicture((prev) => ({
+      ...prev,
+      uploaded: getValues("picture")!,
+    }));
+  }, [getValues("picture")]);
+
+  const onUpload = useCallback(async () => {
+    if (!profilePicture?.notUploaded) return;
+    console.log("Uploading...");
+
+    try {
+      const { objectKey, url } = await UploadAPI.uploadFile(
+        profilePicture.notUploaded.name,
+        "profile-picture"
+      );
+      await UploadAPI.uploadOnS3(url, profilePicture.notUploaded);
+      setProfilePicture({
+        uploaded: objectKey,
+        notUploaded: null,
+      });
+      updateForm("picture", objectKey);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [profilePicture]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setProfilePicture((prev) => ({
+      ...prev,
+      notUploaded: acceptedFiles[0],
+    }));
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/jpeg": [],
+      "image/png": [],
+    },
+    multiple: false,
+  });
 
   const { user } = useUserStore();
+
   if (!user) {
     return;
   }
   const { name, email, username } = user;
-
   return (
     <FormWrapper
       title="Personal info"
       description="Your name, username and email address."
     >
       <div className="w-full flex flex-col gap-5 ">
-        <div className="flex flex-col gap-2">
-          <div className="grid grid-cols-2 gap-5">
-            {(getValues("picture") || tempProfilePicture) && (
+        <div className="flex flex-col gap-2 h-60">
+          <div className="grid grid-cols-2 gap-5 h-full relative">
+            {(profilePicture.notUploaded || profilePicture.uploaded) && (
               <img
-                src={tempProfilePicture || getValues("picture")}
+                src={
+                  profilePicture.notUploaded
+                    ? URL.createObjectURL(profilePicture.notUploaded)
+                    : getObjectURL(profilePicture.uploaded as string)
+                }
                 alt="Profile Picture"
-                className="w-full h-full object-cover rounded-md "
+                className="object-cover w-full rounded-md max-h-60"
               />
             )}
-            <UploadDropzone
-              content={{
-                label: "Drag & Drop / Choose your profile picture",
-              }}
-              endpoint="imageUploader"
+
+            <div
               className={cn(
-                "border dark:border-white/20 mt-0",
-                !(getValues("picture") || tempProfilePicture) && "col-span-full"
+                profilePicture.notUploaded || profilePicture.uploaded
+                  ? "col-span-1"
+                  : "col-span-full"
               )}
-              onClientUploadComplete={(res) => {
-                console.log("Files: ", res);
-                if (res && res?.at(0)?.url) {
-                  updateForm("picture", res[0].url);
-                  setTempProfilePicture(res[0].url);
-                }
-              }}
-              onUploadError={(error: Error) => {
-                alert(`ERROR! ${error.message}`);
-              }}
-            />
+            >
+              <div
+                {...getRootProps()}
+                className={cn(
+                  "h-full border grid place-content-center rounded-md border-dashed cursor-pointer"
+                )}
+              >
+                <input {...getInputProps()} accept="image/*" />
+                {isDragActive ? (
+                  <p>Drop the files here ...</p>
+                ) : (
+                  <>
+                    <p>Drag & drop here, or click to select file</p>
+                    <p className="text-sm text-center text-slate-500">
+                      Accepted file type .jpeg & .png
+                    </p>
+                  </>
+                )}
+              </div>
+              {profilePicture.notUploaded && (
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="relative bottom-10 w-full rounded-t-none"
+                  onClick={onUpload}
+                >
+                  Upload
+                </Button>
+              )}
+            </div>
           </div>
         </div>
         <div className="flex flex-col gap-2">
